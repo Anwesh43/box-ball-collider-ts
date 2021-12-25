9c642192404d1cb3db61c472b419cc823a006cf3
Anwesh43 <anweshthecool0@gmail.com>
2021-12-25 20:23:22 +0530

    adding BBCProps and withContext

---
+++ src/BBCProps.ts
@@ -0,0 +1,8 @@
+interface BBCProps {
+    w : number, 
+    h : number, 
+    scale : number, 
+    onClick : Function
+}
+
+export default BBCProps 
---
+++ src/withContext.tsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import BBCProps from './BBCProps'
+import {useAnimatedScale, useDimension} from './hooks'
+
+const withContext = (MainComponent: React.FC<BBCProps>) : React.FC<BBCProps> => {
+    return () => {
+        const {scale, start : onClick} = useAnimatedScale()
+        const {w, h} = useDimension()
+        const props : BBCProps = {
+            w, 
+            h, 
+            scale,
+            onClick
+        }
+        return (
+            <MainComponent {...props}>
+            </MainComponent>
+        )
+    }
+}
+
+export default withContext
